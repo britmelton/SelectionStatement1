94162137c2441273742fdba43d0bf72e70164e8b
brittany melton <brittanymelton26@gmail.com>
2022-01-12 16:19:06 -0500

    if else favnumber

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //if else statement exercise 
+
+            bool correct = false;   
+            while (!correct)
+            {
+                var r = new Random();
+                var favNumber = r.Next(10);
+                
+                Console.WriteLine("\nGuess my favorite number:");
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("\ntoo low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("\ntoo high");
+                }
+                else 
+                {
+                    correct = true;
+                    Console.WriteLine("\nyou got it!"); 
+                }
+
+
+
+            }
+
+
+            
+        }
+    }
+}
